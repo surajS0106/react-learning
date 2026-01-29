72a3e8fa28e562ee57d1ff8489f2fcac06b6e909
surajS0106 <surajsanjay1999@gmail.com>
2026-01-30 00:46:53 +0530

    feat: initialize React project with TypeScript, ESLint, and Vite
    - Add ESLint configuration for TypeScript and React
    - Create index.html as the entry point for the application
    - Set up package.json with scripts for development, build, lint, and preview
    - Include Vite logo SVG in the public directory
    - Add basic styling in App.css and index.css
    - Implement main App component in App.tsx
    - Add React logo SVG in assets
    - Configure TypeScript with tsconfig files for app and node
    - Set up Vite configuration for React

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function Name() {
+  return <h2>Hi, This is Suraj Sanjay</h2>
+}
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React Tutorial</h1>
+      <Name />
+    </>
+  )
+}
+
+export default App
